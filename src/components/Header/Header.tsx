1ea939b52a096a6f8a17ccd4806aa0781bd88133
Carlos Raúl Cárcamo Tejada <carlosraulcarcamotejada@gmail.com>
2022-06-06 22:57:41 -0600

    Menu de navegación y filtro de búsqueda terminada

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
 
 const Header:FC = () => {
 
@@ -48,16 +49,13 @@ const LogoHeader:FC = () => {
 const MenuButton:FC<{setIsOpen:Function, isOpen:boolean}> = ({setIsOpen,isOpen}) => {
   return (
     <div className="flex">
-      <button type='button' onClick={ () => setIsOpen(!isOpen)} className='px-2 text-gray-500 hover:text-white focus:outline-none focus:text-white'>
-        <svg
-          className="h-6 w-6 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-        <path className={`${isOpen&&'hidden'}`} v-if="!isOpen" fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
-        <path className={`${!isOpen&&'hidden'}`} v-if="isOpen" fillRule="evenodd" clipRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
-
-        </svg>
+      <button type='button' onClick={ () => setIsOpen(!isOpen)} 
+      className='px-2 text-gray-500 hover:text-white 
+                 focus:outline-none focus:text-white'>
+        <div className="h-6 w-6 fill-current" >
+        <MenuIcon className={`${isOpen&&'hidden'}`}/>
+        <XIcon className={`${!isOpen&&'hidden'}`} />
+        </div>
       </button>
     </div>
   );
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,6 @@
 import { useState, FC } from 'react';
+import { SearchIcon, AdjustmentsIcon } from '@heroicons/react/solid';
+
 
 const SearchBar:FC = () => {
 
@@ -21,9 +23,9 @@ const InputSearch:FC = () => {
   return (
     <div className='relative'>
     <div className='absolute inset-y-0 left-0 flex items-center pl-2'>
-      <svg className="h-6 w-6 fill-current text-gray-600" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41l.01-.01zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
+      <div className="h-6 w-6 fill-current text-gray-600" > <SearchIcon /> </div>
     </div>
-    <input className='bg-gray-900 focus:outline-none focus:bg-gray-200 focus:text-gray-900 text-white rounded-lg pl-10 py-2' type="text" placeholder='Search by keywords' />
+    <input className='mt-0 bg-gray-900 focus:outline-none focus:bg-gray-200 focus:text-gray-900 text-white rounded-lg pl-10' type="text" placeholder='Search by keywords' />
   </div>
   )
 }
@@ -31,8 +33,15 @@ const InputSearch:FC = () => {
 const ButtonFilter:FC<{isOpen:boolean,setisOpen:Function}> = ({isOpen,setisOpen}) => {
   return (
     <div className='flex items-center'>
-    <button onClick={()=> setisOpen(!isOpen)} className='inline-flex hover:bg-gray-600 focus:outline-none bg-slate-700 rounded-lg shadow pl-2 pr-4 py-2 hover:text-white ' type='button'>
-      <svg className="h-6 w-6 fill-current text-gray-500 " viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M3 6a1 1 0 0 1 1-1h16a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1zm3 6a1 1 0 0 1 1-1h10a1 1 0 1 1 0 2H7a1 1 0 0 1-1-1zm4 5a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-4z"/></svg>
+    <button 
+    className={`${isOpen?'bg-gray-900':'bg-gray-700'} inline-flex text-gray-500  
+                          hover:bg-gray-600 focus:outline-none  
+                            rounded-lg shadow pl-2 pr-4 ml-4 py-2 hover:text-white`} 
+                            type='button' 
+                            onClick={()=> setisOpen(!isOpen)} 
+    >
+                 
+      <div  className="h-6 w-6 fill-current "> <AdjustmentsIcon /> </div>
       <span className='ml-1 text-white font-medium'>Filters</span>
     </button>
   </div>
@@ -62,9 +71,6 @@ const FilterPanel:FC<{isOpen:boolean}> = ({isOpen}) => {
     {id:5,option:'Kid friendly'},
     {id:6,option:'Townhouse'},
   ]
-
-
-
 
 
   return (
@@ -99,43 +105,37 @@ const FilterPanel:FC<{isOpen:boolean}> = ({isOpen}) => {
       </fieldset>
 
       <fieldset className='px-4 py-4 border-t border-gray-900'>
-
         <span className='block text-sm font-semibold text-gray-500'>Property Type</span>
         {
-          propertyOptions.map( ({option}) => {return (
-            <label className='flex items-center' key={option} htmlFor="">
-            <input className='block' type="radio" name={option.toUpperCase()} id={option.toUpperCase()} value={option}/>
-            <span className='ml-2 text-white'>{option}</span>
-          </label>
+          propertyOptions.map( ({id,option}) => {return (
+            <div className='flex items-center ' key={id}>
+              <input  type="radio" name='propertyT' id={option} value={option}/>
+              <label className='mt-3 ml-2 text-white' key={option} htmlFor='propertyT'>{option}</label>
+            </div>
           )})
         }
-
       </fieldset>
 
       <fieldset className='px-4 py-4 border-t border-gray-900'>
         <span className='block text-sm font-semibold text-gray-500'>Amenities</span>
-
         {
           checkboxOptions.map( ({option}) => {return (
-            <label className='block' key={option} htmlFor="">
-            <input className='' type="checkbox" name="balcony" id="balcony" />
-            <span className='ml-2 text-white'>{option}</span>
-          </label>
+            <div className='flex items-center'  key={option}>
+              <input  type="checkbox" name="balcony" id={option} value={option} />
+              <label className="mt-3 ml-2 text-white">{option}</label>
+            </div>
           )})
         }
-
-
       </fieldset>
-      <div>
-        <button className='mx-2 px-10 w-full bg-blue-400 text-white m-auto' >Update results</button>
+
+      <div className='bg-gray-900 px-4 py-4'>
+        <button className='bg-indigo-500 hover:bg-indigo-400 font-semibold text-white px-4 py-2 rounded-lg w-full'>Update results</button>
       </div>
     </div>
   )
 } 
 
-function classNames(...classes:any) {
-  return classes.filter(Boolean).join(' ')
-}
+
 
 
 
